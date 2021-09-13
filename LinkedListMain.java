a3178247991dbca1c37de131831b715266f7fd79
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-13 21:37:59 +0530

    Added:Program for adding data at the start

---
+++ LinkedListMain.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 to add the data at the start");
+
+        switch (scanner.nextInt()) {
+            case 1:
+                Operations.addeElementAtStart();
+        }
+    }
+}
